0d758889664500c6915ffe1e9f66f22a90a558e7
Seongbin Kim <seongbin9786@gmail.com>
2021-11-06 02:00:09 +0900

    adding usermap + excluding node_modules

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require("@actions/core");
+// const github = require("@actions/github");
+const { Octokit } = require("@octokit/rest"); // 개발 용도로 직접 사용
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const MY_LIMITED_30DAYS_TOKEN = "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";
+const owner = "seongbin9786";
+const repo = "study-nodejs-typescript";
+
+async function run() {
+  console.log("hi !");
+  try {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    //const myToken = core.getInput("myToken");
+    //const octokit = github.getOctokit(myToken);
+    const octokit = new Octokit({
+      auth: MY_LIMITED_30DAYS_TOKEN,
+      userAgent: "participation logger agent v0.0.1",
+      timeZone: "Asia/Seoul",
+      baseUrl: "https://api.github.com",
+    });
+
+    const {
+      data: [lastCommit],
+    } = await octokit.rest.repos.listCommits({
+      owner,
+      repo,
+    });
+    const userName = lastCommit.author.login;
+    // prints last commiter's username
+    // 이 username을 Table의 '실명'으로 대응시키기 위해서는 영구적으로 저장되는 map이 필요할 것이다.
+    // 이 파일은 {repo}/.attendance/usermap.txt 를 읽어 map을 구성하면 될듯하다.
+    console.log(userName);
+
+    octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path,
+    });
+  } catch (error) {
+    // core.setFailed(error.message);
+    console.log(error);
+  }
 }
+
+run();
